19215ad44a78de337f06f3ac214d483efdffa305
alejandroE345 <alejandroencinas345@gmail.com>
2026-04-06 18:33:54 +0000

    porgress

--- script.js
+++ script.js
@@ -4,21 +4,24 @@ console.log("Script Started");
 function start() {
     console.log("click");
     console.log();
-    createCircle();
+    setTimeout(createCircle, 500, 1);
+    setTimeout(createCircle, 1000, 2);
+    setTimeout(createCircle, 1500, 3);
+
 
 }
 
 // Function to create a circle
-function createCircle() {
+function createCircle(num) {
     // Create a div for the circle
     let circle = document.createElement("div");
 
     // Set text and styles
-    circle.innerText = "";
-    circle.style.borderRadius = "25px";
+    circle.innerText = num;
+    circle.style.borderRadius = "40px";
     circle.style.backgroundColor = "blue";
-    circle.style.width = "45px";
-    circle.style.height = "45px";
+    circle.style.width = "80px";
+    circle.style.height = "80px";
     circle.style.textAlign = "center";
     circle.style.alignContent = "center";
     // Position circle randomly using absolute
@@ -30,16 +33,25 @@ function createCircle() {
 
     // Add it to the page
     document.body.appendChild(circle);
+
+    // Make circles clickable
+    circle.addEventListener("click", checkForRemove);
 }
 
 // Function to randomize the circle's X
 function getRandomX() {
-    let randX = Math.random() * window.innerWidth;
+    let randX = Math.random() * (window.innerWidth - 100);
     return randX; 
 }
 
 // Function to randomize the circle's Y
 function getRandomY() {
-    let randY = Math.random * window.innerHeight;
+    let randY = Math.random * (window.innerHeight - 100);
     return randY;
+}
+
+// Checks if circles are clicked in the correct order and removes them
+function checkForRemove(event) {
+    let circle = event.target;
+    console.log(circle.innerText);
 }
